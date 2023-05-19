8a0e06eab02b90b14c619a8f0903a1b6f3830625
Serj <77785989+Smipos@users.noreply.github.com>
2023-05-19 00:55:41 -0400

    Add files via upload

---
+++ lab5/lab5_Mironov.mm
@@ -0,0 +1,124 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="ГОСТ Р 5188-98&#xa;Испытания программных средств на наличие компьютерных вирусов" FOLDED="false" ID="ID_1649577395" CREATED="1684469238064" MODIFIED="1684469524001" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" background="#3c3f41" zoom="0.592">
+    <properties edgeColorConfiguration="#808080ff,#00ddddff,#dddd00ff,#dd0000ff,#00dd00ff,#dd0000ff,#7cddddff,#dddd7cff,#dd7cddff,#7cdd7cff,#dd7c7cff,#7c7cddff" associatedTemplateLocation="template:/Darcula-1.7.mm" fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_286892761" ICON_SIZE="12 pt" COLOR="#cccccc" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_286892761" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#cccccc" BACKGROUND_COLOR="#3c3f41" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#660000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#660000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#dddddd" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff3300">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffb439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#99ffff">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#bbbbbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="21" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Область применения" POSITION="bottom_or_right" ID="ID_1225913821" CREATED="1684469365283" MODIFIED="1684470749511" HGAP_QUANTITY="-49.75 pt" VSHIFT_QUANTITY="-71.25 pt">
+<edge COLOR="#dddd00"/>
+<node TEXT="Распространяется на испытания программных средств и их компонентов, цели которых - обнаружить в этих ПС и устранить из них компьютерные вирусы  силами специальных предприятий, и устанавливает общие требования к организации и проведению таких испытаний" ID="ID_282575186" CREATED="1684470810444" MODIFIED="1684470814078"/>
+<node TEXT="Настоящий стандарт предназначен для применения в испытательных лабораториях, проводящих сертификационные испытания ПС на выполнение требований защиты информации." ID="ID_274660499" CREATED="1684470820414" MODIFIED="1684470829748"/>
+<node TEXT="Настоящий стандарт устанавливает типовые требования, предъявляемые к испытаниям ПС на наличие KB." ID="ID_435778221" CREATED="1684470833157" MODIFIED="1684470843714"/>
+</node>
+<node TEXT="Нормативные ссылки" POSITION="top_or_left" ID="ID_1610601926" CREATED="1684469397676" MODIFIED="1684470892043" HGAP_QUANTITY="40.25 pt" VSHIFT_QUANTITY="-160.5 pt">
+<edge COLOR="#dd0000"/>
+<node TEXT=" В настоящем стандарте использована ссылка на следующий стандарт:&#xa;ГОСТ 19.301-79 Единая система программной документации Программа и методика испытаний. Требования к содержанию и оформлению ." ID="ID_734632780" CREATED="1684470642490" MODIFIED="1684470877861" HGAP_QUANTITY="62.75 pt" VSHIFT_QUANTITY="-87 pt"/>
+</node>
+<node TEXT="Определения и сокращения" POSITION="bottom_or_right" ID="ID_118175779" CREATED="1684469415684" MODIFIED="1684470739630" HGAP_QUANTITY="69.5 pt" VSHIFT_QUANTITY="-3 pt">
+<edge COLOR="#00dd00"/>
+<node TEXT="Защита программных средств - организационные, правовые, технические и технологические меры, направленные на предотвращение возможных несанкционированных действий по отношению к программным средствам и устранение последствий этих действий." ID="ID_1654955202" CREATED="1684470676069" MODIFIED="1684470698230"/>
+<node TEXT="Сертификация - действия третьей стороны, цель которых - подтвердить (с помощью сертификата соответствия) то, что изделие (в том числе программное средство) или услуга соответствует определенным стандартам или другим нормативным документам." ID="ID_1927622987" CREATED="1684470703263" MODIFIED="1684470711024"/>
+<node TEXT="Профилактика - систематические действия эксплуатационного персонала, цель которых - выявить и устранить неблагоприятные изменения в свойствах и характеристиках используемых программных средств, в частности проверить эксплуатируемые, хранимые и (или) вновь полученные программные средства на наличие компьютерных вирусов." ID="ID_1522886063" CREATED="1684470715540" MODIFIED="1684470723695"/>
+<node TEXT="Ревизия - проверка вновь полученных программ специальными средствами, проводимая путем их запуска в контролируемой среде." ID="ID_1393180027" CREATED="1684470754380" MODIFIED="1684470761799"/>
+<node TEXT="Вакцинирование - обработка файлов, дисков, каталогов, проводимая с применением специальных программ, создающих условия, подобные тем, которые создаются определенным компьютерным вирусом, и затрудняющих повторное его появление." ID="ID_1659479444" CREATED="1684470766838" MODIFIED="1684470775276"/>
+<node TEXT="Компьютерный вирус - программа, способная создавать свои копии (необязательно совпадающие с оригиналом) и внедрять их в файлы, системные области компьютера, компьютерных сетей, а также осуществлять иные деструктивные действия." ID="ID_1029826845" CREATED="1684470780071" MODIFIED="1684470788883"/>
+<node TEXT="Сокращения" ID="ID_50499296" CREATED="1684471596191" MODIFIED="1684471599340">
+<node TEXT="ПС - программное средство" ID="ID_892468227" CREATED="1684471600258" MODIFIED="1684471608699"/>
+<node TEXT="КВ -компьютерные вирусы" ID="ID_1084238580" CREATED="1684471610263" MODIFIED="1684471619584"/>
+<node TEXT="ПЭВМ - персональная электронно-вычислительная машина (персональный компьютер)" ID="ID_1935968330" CREATED="1684471621318" MODIFIED="1684471648853"/>
+<node TEXT="ЭВМ - электронно вычислительная машина" ID="ID_787947776" CREATED="1684471650039" MODIFIED="1684471662497"/>
+</node>
+</node>
+<node TEXT="Порядок проведения испытаний компьютерных средств на наличие компьютерных вирусов" POSITION="top_or_left" ID="ID_1659275424" CREATED="1684469447028" MODIFIED="1684470885277" HGAP_QUANTITY="238.24999 pt" VSHIFT_QUANTITY="-42.75 pt">
+<edge COLOR="#dd0000"/>
+<node TEXT="Испытания ПС на наличие KB следует проводить на специально оборудованном программно-аппаратном испытательном стенде, в составе которого должны быть необходимые технические и программные средства, в том числе антивирусные программы." ID="ID_210867968" CREATED="1684470909037" MODIFIED="1684470912230"/>
+<node TEXT="Предприятие [подразделение, проводящее проверку ПС на наличие KB, должно поддерживать испытательный стенд в работоспособном состоянии и не допускать проникновения KB в программы и данные до начала проведения испытаний." ID="ID_1880078578" CREATED="1684470916203" MODIFIED="1684470931528"/>
+<node TEXT="Организация, проводящая проверку ПС на наличие KB, должна определить и зафиксировать в программе испытаний цель и объем испытаний, а также свои обязательства, касающиеся мер защиты проверяемых ПС от их заражения KB." ID="ID_53455545" CREATED="1684470936209" MODIFIED="1684470950235"/>
+<node TEXT="В состав технических средств испытательного стенда должны входить:" ID="ID_1642239674" CREATED="1684471129946" MODIFIED="1684471150920">
+<node TEXT="Совместимые ПЭВМ" ID="ID_855600076" CREATED="1684471168896" MODIFIED="1684471176949"/>
+<node TEXT="Необходимые элементы телекоммуникационных сетей" ID="ID_14254507" CREATED="1684471184409" MODIFIED="1684471201174"/>
+<node TEXT="Каналы связи" ID="ID_1638208477" CREATED="1684471203145" MODIFIED="1684471209860"/>
+</node>
+<node TEXT="Состав и функциональное назначение ПС испытательного стенда определяются системой защиты, применяемой при проведении испытаний ПС на наличие КВ" ID="ID_782199008" CREATED="1684471220115" MODIFIED="1684471270863"/>
+<node TEXT="Проверяемые ПС должны быть переданы для испытаний на магнитных носителях вместе с документацией" ID="ID_1625920111" CREATED="1684471279156" MODIFIED="1684471311206"/>
+</node>
+<node TEXT="Методы проведения испытаний программных средств на наличие компьютерных вирусов" POSITION="bottom_or_right" ID="ID_290000858" CREATED="1684469473898" MODIFIED="1684469540790" VSHIFT_QUANTITY="9.75 pt">
+<edge COLOR="#7cdddd"/>
+<node TEXT="Сканирование" ID="ID_527850835" CREATED="1684471323748" MODIFIED="1684471329293"/>
+<node TEXT="Обнаружение изменений" ID="ID_195303136" CREATED="1684471330762" MODIFIED="1684471337384"/>
+<node TEXT="Эвристический анализ" ID="ID_158946362" CREATED="1684471338791" MODIFIED="1684471346110"/>
+<node TEXT="Резидентные &quot;сторожа&quot;" ID="ID_1789592501" CREATED="1684471347725" MODIFIED="1684471362653"/>
+<node TEXT="Вакцинирование ПС" ID="ID_196244846" CREATED="1684471365114" MODIFIED="1684471375156"/>
+</node>
+<node TEXT="Требования к документации" POSITION="top_or_left" ID="ID_1274693165" CREATED="1684469508314" MODIFIED="1684470887755" HGAP_QUANTITY="194.74999 pt" VSHIFT_QUANTITY="77.25 pt">
+<edge COLOR="#dddd7c"/>
+<node TEXT="Документация, оформляемая при подготовке и проведении испытаний ПС на наличие КВ должна содержать сведения, отражающие цель, объем, порядок проведения и результаты таких испытаний" ID="ID_530844124" CREATED="1684471393197" MODIFIED="1684471454978"/>
+<node TEXT="Выпуск документа вида &quot;Протокол проверки программных средств на отсутствие компьютерных вирусов. Форму документа определяют в установленном порядке и передают в орган по сертификации" ID="ID_1543164141" CREATED="1684471456724" MODIFIED="1684471528288"/>
+<node TEXT="Документация, относящаяся к испытаниям ПС на наличие КВ, может быть представлена на физических носителях данных" ID="ID_24505191" CREATED="1684471538226" MODIFIED="1684471572911"/>
+</node>
+</node>
+</map>
